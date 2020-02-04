59e93199febcb397aefb840b3a27b109ddfca873
Henning Hall <henning.hall@senion.com>
2020-02-04 12:36:47 +0100

    scroll around new try

--- examples/detox/e2e/tests/scrollAround.spec.js
+++ examples/detox/e2e/tests/scrollAround.spec.js
@@ -12,11 +12,12 @@ describe('scroll around', () => {
         await scroll3HoursAndExpect("2000-01-01 03:00:00")
         await scroll3HoursAndExpect("2000-01-01 06:00:00")
         await scroll3HoursAndExpect("2000-01-01 09:00:00")
-        await scroll3HoursAndExpect("2000-01-01 12:00:00")
-        await scroll3HoursAndExpect("2000-01-01 15:00:00")
-        await scroll3HoursAndExpect("2000-01-01 18:00:00")
-        await scroll3HoursAndExpect("2000-01-01 21:00:00")
-        await scroll3HoursAndExpect("2000-01-01 00:00:00")
+        await scrollWheel(1, 4)
+        await expectDate("2000-01-01 13:00:00")
+        await scroll3HoursAndExpect("2000-01-01 16:00:00")
+        await scroll3HoursAndExpect("2000-01-01 19:00:00")
+        await scroll3HoursAndExpect("2000-01-01 22:00:00")
+        await scroll3HoursAndExpect("2000-01-01 01:00:00")
     })
 
     it('Minute wheel should be possible to scroll all way around', async () => {
