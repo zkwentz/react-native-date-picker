429235efea0a2b886fee5b13ee566413404c4aa2
Henning Hall <henning.hall@hotmail.com>
2020-07-27 15:09:59 +0200

    add timezoneoffset tests

---
+++ examples/detox/e2e/tests/timezoneOffset.spec.js
@@ -0,0 +1,79 @@
+const {
+  setTimeZoneOffsetInMinutes,
+  expectDate,
+  scrollWheel,
+  expectDateString,
+  setMaximumDate,
+  setDate,
+} = require('../utils')
+
+const scrollMinuteWheel = () => scrollWheel(2, 1)
+
+describe.only('Minute interval', () => {
+  before(async () => {
+    await device.reloadReactNative()
+    await element(by.text('Advanced')).tap()
+  })
+
+  it('undefined (default)', async () => {
+    await setTimeZoneOffsetInMinutes(undefined)
+    await scrollMinuteWheel()
+    await expectDate('2000-01-01 00:01:00')
+    await expectDateString('Sat Jan 11201 AM ')
+  })
+
+  it('0', async () => {
+    await setTimeZoneOffsetInMinutes(0)
+    await scrollMinuteWheel()
+    await expectDate('2000-01-01 00:01:00')
+    await expectDateString('Fri Dec 311101 PM ')
+  })
+
+  it('180', async () => {
+    await setTimeZoneOffsetInMinutes(180)
+    await scrollMinuteWheel()
+    await expectDate('2000-01-01 00:01:00')
+    await expectDateString('Sat Jan 1201 AM ')
+  })
+
+  it('-180', async () => {
+    await setTimeZoneOffsetInMinutes(-180)
+    await scrollMinuteWheel()
+    await expectDate('2000-01-01 00:01:00')
+    await expectDateString('Fri Dec 31801 PM ')
+  })
+
+  describe('summertime', () => {
+    const firstOfJuly = new Date(2000, 6, 1, 0, 0)
+    const firstOfJune = new Date(2000, 5, 1, 0, 0)
+
+    before(async () => {
+      await setMaximumDate(firstOfJuly)
+      await setDate(firstOfJune)
+    })
+
+    it('undefined', async () => {
+      await setDate(firstOfJune)
+      await setTimeZoneOffsetInMinutes(undefined)
+      await scrollMinuteWheel()
+      await expectDate('2000-06-01 00:01:00')
+      await expectDateString('Thu Jun 11201 AM ')
+    })
+
+    it('0', async () => {
+      await setDate(firstOfJune)
+      await setTimeZoneOffsetInMinutes(0)
+      await scrollMinuteWheel()
+      await expectDate('2000-06-01 00:01:00')
+      await expectDateString('Wed May 311001 PM ')
+    })
+
+    it('180', async () => {
+      await setDate(firstOfJune)
+      await setTimeZoneOffsetInMinutes(180)
+      await scrollMinuteWheel()
+      await expectDate('2000-06-01 00:01:00')
+      await expectDateString('Thu Jun 1101 AM ')
+    })
+  })
+})
